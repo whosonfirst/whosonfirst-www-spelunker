57849b97c06c7b2df194498a79e5f6666606a661
thisisaaronland <aaron@mapzen.com>
2015-08-04 00:37:26 +0000

    more config stuff

--- www/server.py
+++ www/server.py
@@ -13,17 +13,8 @@ from werkzeug.contrib.fixers import ProxyFix
 import math
 import json
 
-# neither of these are properly namespaced/installable yet
-# (20150730/thisisaaronland)
-
 import mapzen.whosonfirst.spatial as spatial
 import mapzen.whosonfirst.search as search
-
-# create lookup/db thingy here
-# create search/db thingy here
-
-pg_search = None
-es_search = None
 
 app = flask.Flask('SPELUNKER')
 app.wsgi_app = ProxyFix(app.wsgi_app)
@@ -36,9 +27,12 @@ def init():
     spatial_dsn = os.environ.get('WOF_SPATIAL_DSN', None)
     spatial_db = spatial.query(query_dsn)
 
-    flask.g.spatial_db = spatial_db
+    search_host = os.environ.get('WOF_SEARCH_HOST', None)
+    search_port = os.environ.get('WOF_SEARCH_PORT', None)
 
-    search_idx = search.query()
+    search_idx = search.query(host=search_host, port=search_port)
+
+    flask.g.spatial_db = spatial_db
     flask.g.search_idx = search_idx
 
 @app.route("/id/<id>")
@@ -134,8 +128,11 @@ if __name__ == '__main__':
     cfg = ConfigParser.ConfigParser()
     cfg.read(options.config)
 
-    dsn = spatial.cfg2dsn(cfg, 'whosonfirst')
+    dsn = spatial.cfg2dsn(cfg, 'spatial')
     os.environ['WOF_SPATIAL_DSN'] = dsn
+
+    os.environ['WOF_SEARCH_HOST'] = cfg.get('search', 'host')
+    os.environ['WOF_SEARCH_PORT'] = cfg.get('search', 'port')
 
     port = int(options.port)
     app.run(port=port)
