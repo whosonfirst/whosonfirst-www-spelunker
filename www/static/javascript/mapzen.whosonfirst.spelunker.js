c19495b8ace0d5aa6328af5b977e7c5f46c84163
thisisaaronland <aaron@mapzen.com>
2015-08-26 00:13:09 +0000

    local proxy settings for venue data

--- www/static/javascript/mapzen.whosonfirst.spelunker.js
+++ www/static/javascript/mapzen.whosonfirst.spelunker.js
@@ -4,13 +4,18 @@ mapzen.whosonfirst = mapzen.whosonfirst || {};
 mapzen.whosonfirst.spelunker = (function(){
 
 	var self = {
-		'toggle_data_endpoint': function(){
-			mapzen.whosonfirst.data.endpoint('https://s3.amazonaws.com/whosonfirst.mapzen.com/data/');
-		},
+		
+		'toggle_data_endpoint': function(placetype){
 
-		'toggle_venue_endpoint': function(){
-			// mapzen.whosonfirst.data.endpoint('https://s3.amazonaws.com/whosonfirst.mapzen.com/data/');
-		}
+			if (placetype == 'venue'){
+				mapzen.whosonfirst.data.endpoint('https://52.27.138.134/venues/');
+			}
+
+			else {
+				mapzen.whosonfirst.data.endpoint('https://s3.amazonaws.com/whosonfirst.mapzen.com/data/');
+			}
+		},
 	};
+
 	return self;
 })();
