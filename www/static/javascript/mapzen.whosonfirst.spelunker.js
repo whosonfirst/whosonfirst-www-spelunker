1446d83c996adea1b42a4bf7351fe5a60c950902
thisisaaronland <aaron@mapzen.com>
2015-08-28 21:58:25 +0000

    proxy through github not S3

--- www/static/javascript/mapzen.whosonfirst.spelunker.js
+++ www/static/javascript/mapzen.whosonfirst.spelunker.js
@@ -7,12 +7,17 @@ mapzen.whosonfirst.spelunker = (function(){
 		
 		'toggle_data_endpoint': function(placetype){
 
+			// mapzen.whosonfirst.data.endpoint('https://s3.amazonaws.com/whosonfirst.mapzen.com/data/');
+
+			var host = location.host;
+			var root = "https://" + host + "/";
+
 			if (placetype == 'venue'){
-				mapzen.whosonfirst.data.endpoint('https://52.27.138.134/venues/');
+				mapzen.whosonfirst.data.endpoint(root + 'venues/');
 			}
 
 			else {
-				mapzen.whosonfirst.data.endpoint('https://s3.amazonaws.com/whosonfirst.mapzen.com/data/');
+				mapzen.whosonfirst.data.endpoint(root + 'data/');
 			}
 		},
 	};
