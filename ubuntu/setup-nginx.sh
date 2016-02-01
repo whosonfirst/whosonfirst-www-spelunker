d66c900b6f74ecfa635d4905f79d48a20e3812e4
thisisaaronland <aaron@mapzen.com>
2016-02-01 22:14:54 +0000

    make setup-nginx work

--- ubuntu/setup-nginx.sh
+++ ubuntu/setup-nginx.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
 
+WHOAMI=`python -c 'import os, sys; print os.path.realpath(sys.argv[1])' $0`
+PARENT=`dirname $WHOAMI`
+
+PROJECT=`dirname $PARENT`
+PROJECT_NAME=`basename ${PROJECT}`
+
+# echo "project ${PROJECT}"
+# echo "project name ${PROJECT_NAME}"
+
 PERL=`which perl`
 
-if [ ! -f /usr/local/mapzen/whosonfirst-www-spelunker/nginx/whosonfirst-www-spelunker.conf ]
+CERTIFIED="${PROJECT}/certified"
+
+if [ ! -f ${PROJECT}/nginx/${PROJECT_NAME}.conf ]
 then
-    cp /usr/local/mapzen/whosonfirst-www-spelunker/nginx/whosonfirst-www-spelunker.conf.example /usr/local/mapzen/whosonfirst-www-spelunker/nginx/whosonfirst-www-spelunker.conf
-    ${PERL} -p -i -e 's!YOUR-SSL-CERTIFICATE-GOES-HERE!/usr/local/mapzen/lockedbox/wof-spelunker.crt!g' /usr/local/mapzen/whosonfirst-www-spelunker/nginx/whosonfirst-www-spelunker.conf
-    ${PERL} -p -i -e 's!YOUR-SSL-CERTFICATE-GOES-HERE!/usr/local/mapzen/lockedbox/wof-spelunker.key!g' /usr/local/mapzen/whosonfirst-www-spelunker/nginx/whosonfirst-www-spelunker.conf
+    cp ${PROJECT}/nginx/${PROJECT_NAME}.conf.example ${PROJECT}/nginx/${PROJECT_NAME}.conf
+
+    # see also: ubuntu/setup-certified*.sh
+
+    if [ -d ${CERTIFIED} ]
+    then
+	${PERL} -p -i -e "s!YOUR-SSL-CERTIFICATE-GOES-HERE!${CERTIFIED}/db/${PROJECT_NAME}.crt!g" ${PROJECT}/nginx/${PROJECT_NAME}.conf
+	${PERL} -p -i -e "s!YOUR-SSL-CERTFICATE-GOES-HERE!${CERTIFIED}/db/${PROJECT_NAME}.key!g" ${PROJECT}/nginx/${PROJECT_NAME}.conf
+    else
+	echo "Can not find ${CERTIFIED} directory so you will need to take care of TLS certs yourself..."
+    fi
+
 fi
 
 if [ -L /etc/nginx/sites-enabled/default ]
@@ -14,17 +34,17 @@ then
     sudo rm /etc/nginx/sites-enabled/default
 fi
 
-if [ -f /etc/nginx/sites-enabled/whosonfirst-www-spelunker.conf ]
+if [ -f /etc/nginx/sites-enabled/${PROJECT_NAME}.conf ]
 then
-    sudo rm /etc/nginx/sites-enabled/whosonfirst-www-spelunker.conf
+    sudo rm /etc/nginx/sites-enabled/${PROJECT_NAME}.conf
 fi
 
-if [ -L /etc/nginx/sites-enabled/whosonfirst-www-spelunker.conf ]
+if [ -L /etc/nginx/sites-enabled/${PROJECT_NAME}.conf ]
 then
-    sudo rm /etc/nginx/sites-enabled/whosonfirst-www-spelunker.conf
+    sudo rm /etc/nginx/sites-enabled/${PROJECT_NAME}.conf
 fi
 
-sudo ln -s /usr/local/mapzen/whosonfirst-www-spelunker/nginx/whosonfirst-www-spelunker.conf /etc/nginx/sites-enabled/whosonfirst-www-spelunker.conf
+sudo ln -s ${PROJECT}/nginx/${PROJECT_NAME}.conf /etc/nginx/sites-enabled/${PROJECT_NAME}.conf
 
 sudo /etc/init.d/nginx restart
 exit 0
