a5fcf2c9350d01f5833c0a305a0f704ce25f050a
thisisaaronland <aaron@mapzen.com>
2016-02-02 17:58:11 +0000

    do not automatically invoke make data; add a make upgrade target; docs

--- ubuntu/setup-elasticsearch-index.sh
+++ ubuntu/setup-elasticsearch-index.sh
@@ -6,10 +6,13 @@ PARENT=`dirname $WHOAMI`
 PROJECT=`dirname $PARENT`
 DATA="${PROJECT}/data"
 
-if [ ! -d ${DATA} ]
-then
-    ${PARENT}/setup-data.sh
-fi
+# This seems like a good idea... until it's not
+# (20160202/thisisaaronland)
+
+# if [ ! -d ${DATA} ]
+# then
+#     ${PARENT}/setup-data.sh
+# fi
 
 /usr/local/bin/wof-es-index -s ${DATA} -b
 
