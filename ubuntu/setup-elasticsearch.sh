51acec810510b5d7e8377e31ca714fe24b078f7d
thisisaaroland <aaron@mapzen.com>
2016-02-05 13:50:38 -0800

    run update before trying to install oracle java

--- ubuntu/setup-elasticsearch.sh
+++ ubuntu/setup-elasticsearch.sh
@@ -23,6 +23,8 @@ PROJECT=`dirname $PARENT`
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/setup-service.html
 
 sudo add-apt-repository ppa:webupd8team/java -y
+
+sudo apt-get update
 sudo apt-get install oracle-java8-installer -y
 
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/setup-repositories.html
@@ -30,7 +32,9 @@ sudo apt-get install oracle-java8-installer -y
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb http://packages.elastic.co/elasticsearch/1.7/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-1.7.list
 
-sudo apt-get update && sudo apt-get install elasticsearch
+sudo apt-get update
+sudo apt-get install elasticsearch
+
 sudo update-rc.d elasticsearch defaults 95 10
 
 if [ ! -f /var/run/elasticsearch/elasticsearch.pid ]
