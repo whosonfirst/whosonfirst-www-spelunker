1563478b0a48feea25e7f28b5688027a9b60014b
thisisaaronland <aaron@mapzen.com>
2016-04-29 16:50:25 +0000

    remove automatic invocation of setup-elasticsearch.sh from setup-app

--- ubuntu/setup-ubuntu.sh
+++ ubuntu/setup-ubuntu.sh
@@ -16,5 +16,3 @@ sudo apt-get install -y gdal-bin
 # of Go because... (20160205/thisisaaronland)
 # sudo apt-get install -y golang
 sudo apt-get install -y make nginx gunicorn python-gevent python-flask
-
-${PARENT}/setup-elasticsearch.sh
