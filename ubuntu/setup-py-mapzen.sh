a2c887c7c3f6ab5bd4ae3295033271d5b2324a00
thisisaaronland <whosonfirst@mapzen.com>
2016-11-03 22:17:06 +0000

    ensure py-mz-wof-sources is installed per issue #55

--- ubuntu/setup-py-mapzen.sh
+++ ubuntu/setup-py-mapzen.sh
@@ -3,6 +3,7 @@
 sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-search/tarball/master
 sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master
 sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master
+sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-sources/tarball/master
 
 # maybe some day but not today because: https://github.com/mapzen/mapzen-www-places/pull/29
 # sudo apt-get install python-pip
