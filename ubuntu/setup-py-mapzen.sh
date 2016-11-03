c4c59cd14995be2c5013a4dd575c5d7b3f0db5cd
thisisaaronland <whosonfirst@mapzen.com>
2016-11-03 21:47:05 +0000

    back out use of pip (rather than easy_install) for py-mapzen, for now

--- ubuntu/setup-py-mapzen.sh
+++ ubuntu/setup-py-mapzen.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-sudo apt-get install python-pip
+sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-search/tarball/master
+sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master
+sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master
 
-sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-search/tarball/master
-sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master
-sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master
+# maybe some day but not today because: https://github.com/mapzen/mapzen-www-places/pull/29
+# sudo apt-get install python-pip
+# sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-search/tarball/master
+# sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master
+# sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master
 
 exit 0
