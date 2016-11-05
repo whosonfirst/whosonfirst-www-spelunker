9fe083638c2ee3b73756cb633fbab29482347a8a
thisisaaronland <whosonfirst@mapzen.com>
2016-11-05 00:40:49 +0000

    update all the pymapzen stuff to use pip... mostly works... I think

--- ubuntu/setup-py-mapzen.sh
+++ ubuntu/setup-py-mapzen.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-search/tarball/master
-sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master
-sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master
-sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-sources/tarball/master
+# the old way that we know works
+# and the new way that we are trying to make sure works...
+# (20161104/thisisaaronland)
 
-# maybe some day but not today because: https://github.com/mapzen/mapzen-www-places/pull/29
-# sudo apt-get install python-pip
-# sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-search/tarball/master
-# sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master
-# sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master
+# sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-search/tarball/master
+# sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master
+# sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master
+# sudo easy_install https://github.com/whosonfirst/py-mapzen-whosonfirst-sources/tarball/master
+
+sudo apt-get install python-pip
+
+sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-search/tarball/master --process-dependency-links
+sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master --process-dependency-links
+sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master --process-dependency-links
+sudo pip install https://github.com/whosonfirst/py-mapzen-whosonfirst-sources/tarball/master --process-dependency-links
 
 exit 0
