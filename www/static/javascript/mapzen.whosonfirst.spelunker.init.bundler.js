6186dbdfd9af1b3ef0ff6a9fa30e1a3e73672fa8
Dan Phiffer <dan@phiffer.org>
2016-11-07 11:35:33 -0500

    :round_pushpin: Optional bundle preview map

--- www/static/javascript/mapzen.whosonfirst.spelunker.init.bundler.js
+++ www/static/javascript/mapzen.whosonfirst.spelunker.init.bundler.js
@@ -8,15 +8,27 @@ window.addEventListener("load", function load(event){
 	var bundler = document.getElementById('wof-bundler');
 	var status = document.getElementById('bundle-status');
 	var parent_id = bundler.getAttribute('data-parent-id');
-	var checkboxes = bundler.querySelectorAll('input[type="checkbox"]');
+	var checkboxes = bundler.querySelectorAll('#wof-bundler input[type="checkbox"]');
 	var btn = document.getElementById('btn-download');
+
+	var bbox = bundler.getAttribute("data-wof-bbox");
+	bbox = bbox.split(",");
+
+	map = mapzen.whosonfirst.leaflet.tangram.map_with_bbox('map', bbox);
+	mapzen.whosonfirst.enmapify.render_id(map, parent_id, function(geojson) {
+		mapzen.whosonfirst.enmapify.render_feature_outline(map, geojson);
+	});
+	window.map = map;
 
 	mapzen.whosonfirst.bundler.set_handler('progress', function(update) {
 		btn.setAttribute('disabled', 'disabled');
 		if (update.type == 'query') {
 			status.innerHTML = 'Looking up ' + update.placetype + ' places (page ' + update.page + ' of ' + update.pages + ')';
 		} else if (update.type == 'feature') {
-			status.innerHTML = 'Downloaded ' + update.count + ' of ' + total + ': ' + update.feature.properties['wof:name'] + ' (' + update.feature.properties['wof:placetype'] + ')';
+			status.innerHTML = 'Downloaded ' + update.count.toLocaleString() + ' of ' + total.toLocaleString() + ': ' + update.feature.properties['wof:name'] + ' (' + update.feature.properties['wof:placetype'] + ')';
+			if (document.getElementById('preview-bundle').checked) {
+				render_feature(update.feature);
+			}
 		}
 	});
 
@@ -41,6 +53,21 @@ window.addEventListener("load", function load(event){
 		}
 	});
 
+	document.getElementById('preview-bundle').addEventListener('change', function(e) {
+		if (e.target.checked) {
+			var bundle = mapzen.whosonfirst.bundler.bundle_features();
+			for (var i in bundle.features) {
+				render_feature(bundle.features[i]);
+			}
+		} else {
+			map.eachLayer(function(layer) {
+				if (layer.wof_id) {
+					map.removeLayer(layer);
+				}
+			});
+		}
+	});
+
 	var include = [];
 	var include_match = window.location.search.match(/include=([^&]+)/);
 	if (include_match) {
@@ -62,6 +89,11 @@ window.addEventListener("load", function load(event){
 		} else {
 			total -= count;
 			mapzen.whosonfirst.bundler.dequeue_placetype(pt);
+			map.eachLayer(function(layer) {
+				if (layer.placetype == pt) {
+					map.removeLayer(layer);
+				}
+			});
 		}
 	};
 
@@ -91,4 +123,45 @@ window.addEventListener("load", function load(event){
 		var filename = 'wof_bundle_' + parent_id + '_' + types + '.geojson';
 		mapzen.whosonfirst.bundler.save_bundle(filename);
 	}, false);
+
+	function render_feature(feature) {
+		var props = feature['properties'];
+		var geom = feature['geometry'];
+
+		var lat = props['geom:latitude'];
+		var lon = props['geom:longitude'];
+
+		var label_text = 'math centroid (shapely) is ';
+		label_text += lat + ", " + lon;
+
+		var pt = {
+			'type': 'Feature',
+			'geometry': { 'type': 'Point', 'coordinates': [ lon, lat ] },
+			'properties': { 'lflt:label_text': label_text }
+		};
+
+		if (geom['type'] == 'Point'){
+
+			var name = props['wof:name'];
+
+			var label_text = name;
+			label_text += ', whose centroid is ';
+			label_text += lat + ", " + lon;
+
+			pt['properties']['lflt:label_text'] = label_text;
+
+			var style = mapzen.whosonfirst.leaflet.styles.math_centroid();
+			var handler = mapzen.whosonfirst.leaflet.handlers.point(style);
+
+			var layer = mapzen.whosonfirst.leaflet.draw_point(map, pt, style, handler);
+		} else {
+
+			feature['properties']['lflt:label_text'] = feature['properties']['wof:name'];
+			var layer = mapzen.whosonfirst.leaflet.draw_poly(map, feature, mapzen.whosonfirst.leaflet.styles.consensus_polygon());
+		}
+
+		layer.wof_id = props['wof:id'];
+		layer.placetype = props['wof:placetype'];
+
+	}
 });
