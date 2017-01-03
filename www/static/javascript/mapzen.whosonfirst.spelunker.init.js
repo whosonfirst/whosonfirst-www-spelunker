fa17baf77704a2b169f255d7d03ef0b841b5cce2
thisisaaronland <whosonfirst@mapzen.com>
2017-01-03 13:45:36 +0000

    invoke mz.wof.chrome in spelunker.init

--- www/static/javascript/mapzen.whosonfirst.spelunker.init.js
+++ www/static/javascript/mapzen.whosonfirst.spelunker.init.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", function load(event){
+
     mapzen.whosonfirst.spelunker.init();
+    mapzen.whosonfirst.chrome.init();
 
     var map = mapzen.whosonfirst.leaflet.tangram.map('map');
 
