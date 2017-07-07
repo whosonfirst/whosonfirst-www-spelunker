4c97438a3b4c366d6a4069d29db29f9ba49e73af
Dan Phiffer <dan@phiffer.org>
2017-07-07 14:21:42 +0800

    :cloud: upload to GitHub Gist
    re: #69

--- www/static/javascript/mapzen.whosonfirst.spelunker.init.download.js
+++ www/static/javascript/mapzen.whosonfirst.spelunker.init.download.js
@@ -28,6 +28,10 @@ window.addEventListener("load", function load(event){
 	var bundle_stats = document.getElementById('bundle-stats');
 	var preview_toggle = document.getElementById('preview-bundle');
 	var simple_props_toggle = document.getElementById('bundle-simple-props');
+	var gist_toggle = document.getElementById('bundle-github-gist');
+
+	var github_access_token = null;
+	var bundle_is_ready = false;
 
 	var root = document.body.getAttribute("data-abs-root-url");
 	var simple_props_lookup = null;
@@ -115,16 +119,90 @@ window.addEventListener("load", function load(event){
 		return ret;
 	});
 
+	function get_filename() {
+		var types = get_chosen_types().join('-');
+		var simple = simple_props_toggle.checked ? '_simple' : '';
+		var parent_name = bundler.getAttribute('data-parent-name');
+		parent_name = parent_name.toLowerCase();
+		parent_name = parent_name.replace(/\W+/, '-');
+		var filename = 'wof_bundle_' + parent_name + '_' + parent_id + '_' + types + simple + '.geojson';
+		return filename;
+	}
+
+	var github_interval = null;
+	function upload_bundle_to_gist() {
+
+		var github = document.getElementById('bundle-github');
+		github.className = 'uploading';
+
+		var xhr = new XMLHttpRequest();
+		var url = "https://api.github.com/gists";
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-type", "application/json");
+		xhr.setRequestHeader("Accept", "application/vnd.github.v3+json");
+		xhr.setRequestHeader("Authorization", "token " + github_access_token);
+
+		var files = {};
+
+		var filename = get_filename();
+		files[filename] = {
+			content: JSON.stringify(mapzen.whosonfirst.bundler.bundle_features())
+		};
+
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4 && xhr.status == 201) {
+				var done = document.getElementById('bundle-github-done');
+				var rsp = JSON.parse(xhr.responseText);
+				var gist_url = rsp.files[filename].raw_url;
+				done.innerHTML = '<strong><a href="' + mapzen.whosonfirst.php.htmlspecialchars(gist_url) + '">Your GitHub Gist is ready</a></strong>';
+				github.className = 'done';
+			}
+		}
+
+		var req = JSON.stringify({
+			"description": "WOF bundle: " + get_chosen_types().join(', ') + ' descendants of ' + bundler.getAttribute('data-parent-name'),
+			"public": true,
+			"files": files
+		});
+		xhr.send(req);
+	}
+
+	function wait_for_github_login() {
+		if (! github_interval) {
+			github_interval = setInterval(function() {
+				localforage.getItem('github_access_token').then(function(rsp) {
+					if (rsp) {
+						var github = document.getElementById('bundle-github');
+						github.className = 'logout';
+						github_access_token = rsp;
+						clearInterval(github_interval);
+						github_interval = null;
+						if (bundle_is_ready) {
+							upload_bundle_to_gist();
+						}
+					}
+				});
+			}, 1000);
+		}
+	}
+
 	mapzen.whosonfirst.bundler.set_handler('bundle_ready', function(update) {
 		document.getElementById('bundle-btns').className = '';
 		document.getElementById('stats').className = '';
+		document.getElementById('cancel-download').className = 'hidden';
 		if (update.bundle.features.length != total) {
 			status.innerHTML = '<i>The number of items in your bundle (' + bundle_count.toLocaleString() + ') does not match the expected ' + total.toLocaleString() + '.</i>';
 		} else {
 			status.innerHTML = '';
 		}
-		//bundle_stats.innerHTML = 'GeoJSON bundle: ' + display_filesize(update.bundle_size);
-		//summary_stats.innerHTML = 'CSV summary: ' + display_filesize(update.summary_size);
+		if (gist_toggle.checked) {
+			if (github_access_token) {
+				upload_bundle_to_gist();
+			} else if (! github_interval) {
+				wait_for_github_login(upload_bundle_to_gist);
+			}
+		}
+		bundle_is_ready = true;
 	});
 
 	mapzen.whosonfirst.bundler.set_handler('error', function(details) {
@@ -197,11 +275,9 @@ window.addEventListener("load", function load(event){
 			if (total > mapzen.whosonfirst.bundler.feature_count_limit) {
 				document.getElementById('selected-count').innerHTML += '<br><i>Please note that you currently cannot download more than ' + mapzen.whosonfirst.bundler.feature_count_limit.toLocaleString() + ' features of a given placetype at a time. This is not by design, and we are working to remove the limit.</i><br>';
 			}
-			document.getElementById('start-btn').className = '';
-			document.getElementById('bundle-controls').className = 'active';
+			btn_start.className = 'btn btn-mapzen btn-primary';
 		} else {
-			document.getElementById('start-btn').className = 'hidden';
-			document.getElementById('bundle-controls').className = '';
+			btn_start.className = 'btn btn-mapzen btn-primary disabled';
 		}
 	};
 
@@ -220,6 +296,10 @@ window.addEventListener("load", function load(event){
 	simple_props_toggle.removeAttribute('disabled');
 
 	btn_start.addEventListener('click', function(e) {
+		e.preventDefault();
+		if (btn_start.className.indexOf('disabled') !== -1) {
+			return;
+		}
 		status.innerHTML = 'Preparing to bundle';
 		mapzen.whosonfirst.bundler.bundle();
 		document.getElementById('start-btn').className = 'hidden';
@@ -229,6 +309,18 @@ window.addEventListener("load", function load(event){
 		simple_props_toggle.setAttribute('disabled', 'disabled');
 		document.getElementById('cancel-download').className = '';
 		document.getElementById('output').className = '';
+
+		if (gist_toggle.checked) {
+			var github = document.getElementById('bundle-github');
+			localforage.getItem('github_access_token').then(function(rsp) {
+				if (! rsp) {
+					github.className = 'login';
+				} else {
+					github_access_token = rsp;
+					github.className = 'logout';
+				}
+			});
+		}
 	});
 
 	document.getElementById('cancel-download').addEventListener('click', function(e) {
@@ -238,19 +330,31 @@ window.addEventListener("load", function load(event){
 		document.getElementById('start-btn').className = '';
 		document.getElementById('cancel-download').className = 'hidden';
 		document.getElementById('output').className = 'hidden';
-	});
+	}, false);
+
+	document.getElementById('github-login').addEventListener('click', function(e) {
+		e.preventDefault();
+		window.open(root + 'auth', 'auth', 'width=640,height=480');
+		wait_for_github_login();
+	}, false);
+
+	document.getElementById('github-logout').addEventListener('click', function(e) {
+		e.preventDefault();
+		var github = document.getElementById('bundle-github');
+		github.className = 'login';
+		localforage.removeItem('github_access_token');
+		if (github_interval) {
+			clearInterval(github_interval);
+			github_interval = null;
+		}
+	}, false);
 
 	btn_bundle.addEventListener('click', function(e) {
 		e.preventDefault();
 		if (btn_bundle.getAttribute('disabled') == 'disabled') {
 			return;
 		}
-		var types = get_chosen_types().join('-');
-		var simple = simple_props_toggle.checked ? '_simple' : '';
-		var parent_name = bundler.getAttribute('data-parent-name');
-		parent_name = parent_name.toLowerCase();
-		parent_name = parent_name.replace(/\W+/, '-');
-		var filename = 'wof_bundle_' + parent_name + '_' + parent_id + '_' + types + simple + '.geojson';
+		var filename = get_filename();
 		mapzen.whosonfirst.bundler.save_bundle(filename);
 	}, false);
 
@@ -331,18 +435,5 @@ window.addEventListener("load", function load(event){
 			return (bytes / (1024 * 1024)).toFixed(precision) + ' MB';
 		}
 	}
-
-	var gist_toggle = document.getElementById('bundle-github-gist');
-	gist_toggle.addEventListener('change', function(e) {
-		if (gist_toggle.checked) {
-			localforage.getItem('github_access_token').then(function(rsp) {
-				if (! rsp) {
-					window.open(root + 'auth', 'auth', 'width=640,height=480');
-				} else {
-					console.log('access_token: ', rsp);
-				}
-			});
-		}
-	});
 
 });
